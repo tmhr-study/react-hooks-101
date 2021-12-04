216f3911ffdfa8474bcc502248b0787c5dd3bb34
tmhr-study <tmhr.hsgw.p@gmail.com>
2021-12-04 14:26:22 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount(previousCount=>previousCount + 1);
+
   return (
-    <div>
-      This is a Template for React app.
-    </div>
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+      </div>
+    </>
   );
 }
 
